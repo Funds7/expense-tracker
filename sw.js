598f0ef811ede3a6ce27e3fda1a376acc0f76183
Funds7 <odigwejoshua7@gmail.com>
2026-05-01 22:30:49 +0100

    Improve service worker install and activate handling
    Added skipWaiting and clients.claim to improve service worker lifecycle management.

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker Installed");
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
